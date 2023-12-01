cb715c46aeecd27c348e91e633de574c486fb301
anazalu <109597957+anazalu@users.noreply.github.com>
2023-12-01 13:33:16 +0200

    Update date-generator.ts

--- cypress/fixtures/date-generator.ts
+++ cypress/fixtures/date-generator.ts
@@ -1,12 +1,12 @@
-const dateObject = new Date(Date.now());
+const dateObject: Date = new Date(Date.now());
 
-const year = dateObject.getFullYear();
-const month = ('0' + (dateObject.getMonth() + 1)).slice(-2);
-const day = ('0' + dateObject.getDate()).slice(-2);
-const hours = ('0' + dateObject.getHours()).slice(-2);
-const minutes = ('0' + dateObject.getMinutes()).slice(-2);
-const seconds = ('0' + dateObject.getSeconds()).slice(-2);
+const year: number = dateObject.getFullYear();
+const month: string = ('0' + (dateObject.getMonth() + 1)).slice(-2);
+const day: string = ('0' + dateObject.getDate()).slice(-2);
+const hours: string = ('0' + dateObject.getHours()).slice(-2);
+const minutes: string = ('0' + dateObject.getMinutes()).slice(-2);
+const seconds: string = ('0' + dateObject.getSeconds()).slice(-2);
 
-const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+const formattedDate: string = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
 export default formattedDate;
