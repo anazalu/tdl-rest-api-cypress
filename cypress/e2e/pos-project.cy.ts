3cd5eaf8d8bf064bed5bd6c735f245878fa19b39
anazalu <109597957+anazalu@users.noreply.github.com>
2023-12-01 15:29:39 +0200

    Update pos-project.cy.ts

--- cypress/e2e/pos-project.cy.ts
+++ cypress/e2e/pos-project.cy.ts
@@ -1,35 +1,13 @@
 import { config } from "../../config"
+import { generateFormattedDate } from '../utils/dateUtils';
 
-const dateObject = new Date(Date.now());
-
-const year = dateObject.getFullYear();
-const month = ('0' + (dateObject.getMonth() + 1)).slice(-2);
-const day = ('0' + dateObject.getDate()).slice(-2);
-const hours = ('0' + dateObject.getHours()).slice(-2);
-const minutes = ('0' + dateObject.getMinutes()).slice(-2);
-const seconds = ('0' + dateObject.getSeconds()).slice(-2);
-
-const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+const formattedDate = generateFormattedDate();
 
 describe('Single project tests', () => {
-  let projectId: string;
-
-
-  before(() => {
-    // retrieve a project ID
-    cy.request({
-      method: 'GET',
-      url: '/projects',
-      headers: {
-        Authorization: `Bearer ${config.token}`
-      }
-    }).then((response) => {
-      expect(response.status).to.equal(200);
-      expect(response.body).to.have.length.greaterThan(0);
-      expect(response.body[0]).to.have.property('id');
-      projectId = response.body[response.body.length - 1].id;
+  
+    before(() => {
+        cy.getProjectId();
     });
-  });
 
   it('PATCH should modify name and description of a project using the /projects{id} endpoint', () => {
 
@@ -46,7 +24,6 @@ describe('Single project tests', () => {
     })
       .then((response) => {
         expect(response.status).to.equal(204);
-
       });
   })
 });
