93823a966d94fc7abea920bca6a057299e73c57c
anazalu <109597957+anazalu@users.noreply.github.com>
2023-12-01 15:22:05 +0200

    Update pos-colors.cy.ts

--- cypress/e2e/pos-colors.cy.ts
+++ cypress/e2e/pos-colors.cy.ts
@@ -1,23 +1,10 @@
 import { config } from "../../config"
 
 describe('Colors tests', () => {
-  let projectId: string;
-
-  before(() => {
-    // retrieve a project ID
-    cy.request({
-      method: 'GET',
-      url: '/projects',
-      headers: {
-        Authorization: `Bearer ${config.token}`
-      }
-    }).then((response) => {
-      expect(response.status).to.equal(200);
-      expect(response.body).to.have.length.greaterThan(0);
-      expect(response.body[0]).to.have.property('id');
-      projectId = response.body[response.body.length - 1].id;
+  
+    before(() => {
+        cy.getProjectId();
     });
-  });
 
   it('GET should fetch either an empty list or colors from the /projects/{id}/colors endpoint', () => {
     cy.request({
