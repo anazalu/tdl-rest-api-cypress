df96fc14bdb7e2c2c271506ac0cce34d3331efc4
anazalu <109597957+anazalu@users.noreply.github.com>
2023-12-01 15:59:31 +0200

    Create dateUtils.ts

---
+++ cypress/utils/dateUtils.ts
@@ -0,0 +1,14 @@
+const generateFormattedDate = () => {
+  const dateObject: Date = new Date(Date.now());
+  
+  const year: number = dateObject.getFullYear();
+  const month: string = ('0' + (dateObject.getMonth() + 1)).slice(-2);
+  const day: string = ('0' + dateObject.getDate()).slice(-2);
+  const hours: string = ('0' + dateObject.getHours()).slice(-2);
+  const minutes: string = ('0' + dateObject.getMinutes()).slice(-2);
+  const seconds: string = ('0' + dateObject.getSeconds()).slice(-2);
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
+export { generateFormattedDate };
